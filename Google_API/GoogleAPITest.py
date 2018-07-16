6f53887cfec90de8e13281c1ede5317c426a961e
LincolnK <lincoln.kinley@siu.edu>
2018-07-16 18:23:15 -0500

    Add files via upload

--- Google_API/GoogleAPITest.py
+++ Google_API/GoogleAPITest.py
@@ -12,34 +12,13 @@ CV_WINDOW_NAME = "Simple Video Viewer"
 FILENAME = "5ft_test.mp4"
 
 def detect_face(image, max_results=4):
-    """Uses the Vision API to detect faces in the given file.
-    Args:
-        face_file: A file-like object containing an image with faces.
-    Returns:
-        An array of Face objects with information about the picture.
-    """
-    # [START get_vision_service]
     client = vision.ImageAnnotatorClient()
-    # [END get_vision_service]
-
-    #content = face_file.read()
     content = image
     image = types.Image(content=content)
-
     return client.face_detection(image=image).face_annotations
 
-def highlight_faces(image, faces, output_filename):
-    """Draws a polygon around the faces, then saves to output_filename.
-    Args:
-      image: a file containing the image with the faces.
-      faces: a list of faces found in the file. This should be in the format
-          returned by the Vision API.
-      output_filename: the name of the image file to be created, where the
-          faces have polygons drawn around them.
-    """
-    #im = Image.open(image)
-    #draw = ImageDraw.Draw(image)
 
+def highlight_faces(image, faces, output_filename):
     for face in faces:
         box = [(vertex.x, vertex.y) for vertex in face.bounding_poly.vertices]
         coords = []
@@ -51,8 +30,7 @@ def highlight_faces(image, faces, output_filename):
         h = coords[2][1]-y
         if(w < 0):
             x = x + w
-            w = -1 * w
-            
+            w = -1 * w   
         if(h < 0):
             y = y + h
             h = -1 * h
@@ -79,10 +57,8 @@ def main():
             break
         p_img = cv2.imencode('.jpg', video_image, [int(cv2.IMWRITE_JPEG_QUALITY), 100])[1].tostring()
         if(set):
-            print(type(p_img))
             x = detect_face(p_img,4)
             print('Found {} face{}'.format(len(x), '' if len(x) == 1 else 's'))
-            print(x)
             highlight_faces(video_image, x, "outfile.jpg")
             set = False
         cv2.imshow(CV_WINDOW_NAME, video_image)
